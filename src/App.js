2fb59fd1ad4d0f8b8a364b8d75c0cf8fa78d8627
hoangquan204 <quanpogba888@gmail.com>
2026-04-16 12:47:24 +0700

    Fix loi vercel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
-import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Pages/Dashboard";
-
-function Home() {
-  return <h1>Home Page</h1>;
-}
-
-function About() {
-  return <h1>About Page</h1>;
-}
-
 function App() {
   return (
     <Dashboard />
